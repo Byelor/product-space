341b35f91f99f06c134cb3e74b966945d0f796da
Byelor <zaqw1234bmbmbm@gmail.com>
2026-04-22 21:54:11 +0300

    feat:add-express-hbs

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
 import express from "express";
-import hbs from "hbs";
+import hbs from "express-hbs";
 import path from "path";
 import pool from "./db/postgres.js";
 
 const __dirname = import.meta.dirname;
 
-hbs.registerPartials(path.join(__dirname, "../views/partials"));
 const app = express();
-
+app.engine('hbs', hbs.express4({partialsDir: path.join(__dirname, "../views/partials")}));
 app.set('view engine', 'hbs');
 app.use(express.static("../static"));
 
