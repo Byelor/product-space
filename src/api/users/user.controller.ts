eca7b57f82c2f17f3ae66036c4c1308fdda7f7ab
Byelor <zaqw1234bmbmbm@gmail.com>
2026-04-23 23:31:44 +0300

    feat:user-controller

--- src/api/users/user.controller.ts
+++ src/api/users/user.controller.ts
@@ -3,6 +3,8 @@ import express from "express";
 
 class UserController{
     async getAll(req: express.Request, res: express.Response){
-        
+        const userArray = await userService.getAll();
+        res.json(userArray);
     }
 }
+export default new UserController();
